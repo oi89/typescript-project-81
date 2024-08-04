2dedffac1677f73b14f1b1fd0972c8eb7c4b36e6
Oleg Ivanov <olegivanov1989@gmail.com>
2024-08-04 13:19:29 +0300

    Add class for generate tags

---
+++ TagFactory.ts
@@ -0,0 +1,29 @@
+import { Tags } from './Tags.data';
+
+export class TagFactory {
+    constructor(
+        private tag: Tags,
+        private attributes?: object,
+        private text?: string
+    ) {}
+
+    toString() {
+        let attributesBlock = '';
+        let closeTag = '';
+        const attributeText = this.text ?? '';
+
+        if (this.attributes) {
+            let attributesArr = [];
+            for (let [attribute, value] of Object.entries(this.attributes)) {
+                attributesArr.push(` ${attribute}="${value}"`);
+            }
+            attributesBlock = attributesArr.join('');
+        }
+
+        if (this.tag === Tags.FORM || this.tag === Tags.LABEL || this.tag === Tags.TEXTAREA) {
+            closeTag = `</${this.tag}>`;
+        }
+
+        return `<${this.tag}${attributesBlock}>${attributeText}${closeTag}`;
+    }
+}
---
+++ Tags.data.ts
@@ -0,0 +1,8 @@
+export enum Tags {
+    FORM = 'form',
+    LABEL = 'label',
+    INPUT = 'input',
+    TEXTAREA = 'textarea',
+    BR = 'br',
+    IMG = 'img'
+}
